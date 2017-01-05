3b53239f56fd3dec60787f5ad20cab8736e9a44c
Pontakorn Paesaeng <pontakornpaesaeng@gmail.com>
2017-01-05 18:21:07 +0700

    Use ManyToManyField instead

--- entry/models.py
+++ entry/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.urls import reverse
-from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 # Create your models here.
 class Entry(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=1300)
-    tags = TaggableManager()
     created_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
     writer = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tags')
     def __str__(self):
         return self.title
     def get_absolute_url(self):
         return reverse('entry:detail',args=(self.pk,))
+class Tags(models.Model):
+    label = models.CharField(max_length=20)
